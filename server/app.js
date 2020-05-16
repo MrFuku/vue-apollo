e8e2eae1fabe9d9ef6c6dca82f491828a903f0c7
MrFuku <mmafighter_shun@icloud.com>
2020-05-16 21:16:07 +0900

    ✨mondodbへの接続
    Auto stash before rebase of "refs/heads/master"

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const mongoose = require('mongoose')
+const schema = require('./schema/schema')
 const app = express()
 
+const user = 'user'
+const pass = 'password'
+mongoose.connect(`mongodb://${user}:${pass}@mongo/test`)
+mongoose.connection.once('open', () => {
+  console.log('connected mongodeb')
+})
 app.use('/graphql', graphqlHTTP({
-  
-}))
+    schema,
+    graphiql: true,
+  })
+)
 app.listen(4000, () => {
   console.log('listening port 4000')
 })
