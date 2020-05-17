df1f619c547def037a4835a75afc5c9c63d8116d
MrFuku <mmafighter_shun@icloud.com>
2020-05-17 14:10:28 +0900

    apollo-clientインストール

--- front/src/main.js
+++ front/src/main.js
@@ -2,12 +2,31 @@ import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+import App from './App.vue'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+Vue.use(VueApollo)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
