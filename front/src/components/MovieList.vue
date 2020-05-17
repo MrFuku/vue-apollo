517fee32c17745592ee83bf58c835c827ddca075
MrFuku <mmafighter_shun@icloud.com>
2020-05-17 12:55:42 +0900

    映画一覧表示用にテーブルを作成

--- front/src/components/MovieList.vue
+++ front/src/components/MovieList.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    movie list
+    <b-table-simple
+      outlined
+      hover
+      class="mt-4"
+    >
+      <b-thead>
+        <b-tr>
+          <b-th>タイトル</b-th>
+          <b-th>ジャンル</b-th>
+          <b-th>監督</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr>
+          <b-td>となりのトトロ</b-td>
+          <b-td>アニメ</b-td>
+          <b-td>宮崎 駿</b-td>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "MovieList"
-  }
+export default {
+  name: "MovieList"
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
